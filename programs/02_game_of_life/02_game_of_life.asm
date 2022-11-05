9117ef664654aa4b6fe208de8035b654bc7209e4
Jacob Still <jacobcstill@gmail.com>
2022-11-05 15:52:16 -0700

    fix iteratio

--- programs/02_game_of_life/02_game_of_life.asm
+++ programs/02_game_of_life/02_game_of_life.asm
@@ -1,29 +1,29 @@
-init:
+;init:
         ; set the led matrix to display page C+1 and C
-        mov     R0,0xC          ; 0x90C         // set R0 to 0xC
-        mov     [0xF0],R0       ; 0xCF0         // move value in R0 to data address 0xF0
+        ;mov     R0,0xC          ; 0x90C         // set R0 to 0xC
+        ;mov     [0xF0],R0       ; 0xCF0         // move value in R0 to data address 0xF0
         
-        ; init for random data
-        mov     R0,[0xF0]       ; 0xDF0         // get the display address
-        mov     R9,0x6          ; 0x996         // move 0x6 address to R9
-        xor     R9,R0           ; 0x790         // xor the display address in R0 and 0x6 in R0 to get the "procssing address" (0xA in this case)
-        mov     R6,R0           ; 0x860         // move processing address into R6
-        mov     R5,0x0          ; 0x950         // load 0 to R5
+        ;; init for random data
+        ;mov     R0,[0xF0]       ; 0xDF0         // get the display address
+        ;mov     R9,0x6          ; 0x996         // move 0x6 address to R9
+        ;xor     R9,R0           ; 0x790         // xor the display address in R0 and 0x6 in R0 to get the "procssing address" (0xA in this case)
+        ;mov     R6,R0           ; 0x860         // move processing address into R6
+        ;mov     R5,0x0          ; 0x950         // load 0 to R5
 
-        ; write random data to the display
-        mov     R0,[0xFF]       ; 0xDFF         // read from RNG
-        mov     [R6:R5],R0      ; 0xA64         // write to cell (in memory)
-        inc     R5              ; 0x025         // increment R5
-        skip    c,1             ; 0x0F1         // skip if carry
-        jr      -5              ; 0xFFB         // jump back to do the rest of the first page
-        inc     R6              ; 0x026         // increment R6
-        mov     R0,[0xFF]       ; 0xDFF         // read from RNG
-        mov     [R6:R5],R0      ; 0xA64         // write to cell (in memory)
-        inc     R5              ; 0x025         // increment R5
-        skip    c,1             ; 0x0F1         // skip if carry
-        jr      -5              ; 0xFFB         // jump back to do the rest of the second page
+        ;; write random data to the display
+        ;mov     R0,[0xFF]       ; 0xDFF         // read from RNG
+        ;mov     [R6:R5],R0      ; 0xA64         // write to cell (in memory)
+        ;inc     R5              ; 0x025         // increment R5
+        ;skip    c,1             ; 0x0F1         // skip if carry
+        ;jr      -5              ; 0xFFB         // jump back to do the rest of the first page
+        ;inc     R6              ; 0x026         // increment R6
+        ;mov     R0,[0xFF]       ; 0xDFF         // read from RNG
+        ;mov     [R6:R5],R0      ; 0xA64         // write to cell (in memory)
+        ;inc     R5              ; 0x025         // increment R5
+        ;skip    c,1             ; 0x0F1         // skip if carry
+        ;jr      -5              ; 0xFFB         // jump back to do the rest of the second page
         
-        ret     R0,0
+        ;ret     R0,0
 
 
 
@@ -58,6 +58,15 @@ init:
 ;        mov     PCM,0x0         ; 0x9E0         // set PCM to 0x0
 ;        mov     PCL,0x2         ; 0x9D2         // set PCL to 0x2
 
+        mov R0,0xC
+        mov [0xF0],R0
+
+        GOSUB   iter
+        ret     R0,0
+
+
+
+
 ; iter subroutine
 ; [0xF0] - page n
 ; R9 - dest
@@ -73,18 +82,18 @@ iter_row:
 iter_bit1:
         rrc     R7              ; Roll R7 with carry
         skip    nc,1            ; skip if jump if looped
-        jr      6
+        jr      5
         mov     R6,1            ; start with page n + 1
         GOSUB   rules           ; jump to ckeck bit in page n + 1
         GOTO    iter_bit1       ; jump back to top
 iter_bit0:
         rrc     R7              ; Roll R7 with carry
         skip    nc,1            ; skip if jump if looped
-        jr      6
+        jr      5
         mov     R6,0            ; page n
         GOSUB   rules           ; jump to ckeck bit in page n
         GOTO    iter_bit0       ; jump back to top
-        inc     R7              ; increment col
+        inc     R8              ; increment col
         skip    c,2             ; skip GOTO if cary
         GOTO    iter_row        ; GOTO top of row iteration
         ret     R0,0            ; end - change to return from subroutine
@@ -93,15 +102,23 @@ iter_bit0:
 
 ; rules subroutine
 rules:
-        jr 0            ; placholder
+        ;jr 0            ; placholder
 
+        ;mov     R0,[R5:R8]      ; get data at memory
+        ;and     R0,R7           ; check if bit is set
+        ;GOSUB   rules           ; jump to check bit
+        ;skip    NZ, 1
+        ;jr      -#
 
+        mov     R0,[0xF0]
+        add     R0,R6
+        mov     R3,R0
+        mov     R0,[R3:R8]
+        or      R0, r7
+        mov     [R3:R8],R0
 
-        mov     R0,[R5:R8]      ; get data at memory
-        and     R0,R7           ; check if bit is set
-        GOSUB   rules           ; jump to check bit
-        skip    NZ, 1
-        jr      -#
+        ret     R0,0
+
 
 
 
