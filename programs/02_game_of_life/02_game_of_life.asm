bad6ef31a500f3b204325bb9dba30c6443d48625
Jacob Still <jacobcstill@gmail.com>
2022-11-06 14:10:36 -0800

    update iteration a bit

--- programs/02_game_of_life/02_game_of_life.asm
+++ programs/02_game_of_life/02_game_of_life.asm
@@ -1,8 +1,8 @@
 ;init:
-        ; set the led matrix to display page C+1 and C
+        ;; set the led matrix to display page C+1 and C
         ;mov     R0,0xC          ; 0x90C         // set R0 to 0xC
         ;mov     [0xF0],R0       ; 0xCF0         // move value in R0 to data address 0xF0
-        
+
         ;; init for random data
         ;mov     R0,[0xF0]       ; 0xDF0         // get the display address
         ;mov     R9,0x6          ; 0x996         // move 0x6 address to R9
@@ -11,18 +11,23 @@
         ;mov     R5,0x0          ; 0x950         // load 0 to R5
 
         ;; write random data to the display
-        ;mov     R0,[0xFF]       ; 0xDFF         // read from RNG
-        ;mov     [R6:R5],R0      ; 0xA64         // write to cell (in memory)
-        ;inc     R5              ; 0x025         // increment R5
-        ;skip    c,1             ; 0x0F1         // skip if carry
-        ;jr      -5              ; 0xFFB         // jump back to do the rest of the first page
-        ;inc     R6              ; 0x026         // increment R6
-        ;mov     R0,[0xFF]       ; 0xDFF         // read from RNG
-        ;mov     [R6:R5],R0      ; 0xA64         // write to cell (in memory)
-        ;inc     R5              ; 0x025         // increment R5
-        ;skip    c,1             ; 0x0F1         // skip if carry
-        ;jr      -5              ; 0xFFB         // jump back to do the rest of the second page
-        
+        ;mov     R0,[0xFF]       ; Read random number
+        ;mov     [R6:R5],R0      ; Move Random into page n
+        ;mov     R7,R6           ; copy page n
+        ;inc     R7              ; get page n + 1
+        ;mov     R0,[0xFF]       ; Read Random number
+        ;mov     [R7:R5],R0      ; Move Random into page n + 1
+        ;inc     R5              ; Move to next row
+        ;skip    C,1             ; Skip after row 15
+        ;jr      -9              ; Loop forever
+
+
+
+
+
+
+
+
         ;ret     R0,0
 
 
@@ -61,6 +66,9 @@
         mov R0,0xC
         mov [0xF0],R0
 
+        mov R0,8
+        mov [0xf1],R0
+
         GOSUB   iter
         ret     R0,0
 
@@ -82,16 +90,18 @@ iter_row:
 iter_bit1:
         rrc     R7              ; Roll R7 with carry
         skip    nc,1            ; skip if jump if looped
-        jr      5
+        jr      6
         mov     R6,1            ; start with page n + 1
         GOSUB   rules           ; jump to ckeck bit in page n + 1
+        and     R0,0xf          ; clear carry
         GOTO    iter_bit1       ; jump back to top
 iter_bit0:
         rrc     R7              ; Roll R7 with carry
         skip    nc,1            ; skip if jump if looped
-        jr      5
+        jr      6
         mov     R6,0            ; page n
         GOSUB   rules           ; jump to ckeck bit in page n
+        and     R0,0xf          ; clear carry
         GOTO    iter_bit0       ; jump back to top
         inc     R8              ; increment col
         skip    c,2             ; skip GOTO if cary
@@ -99,26 +109,46 @@ iter_bit0:
         ret     R0,0            ; end - change to return from subroutine
 
 
+
+
+setter:
+        mov     R0,[0xF0]
+        add     R0,R6
+        mov     R3,R0
+        mov     R0,[R3:R8]
+        or      R0,r7
+        mov     [R3:R8],R0
+        ret     R0,0
+
+
+
 ; rules subroutine
+; [0xF0] - page n
+; R9 - dest
+; R8 - row
+; R7 - col
+; R6 - page offset
 ; R2 - edge cond nibble (b3 b2 b1 b0) -> (Top Bottom Left Right)
 rules:
+        GOSUB   setter
+
         mov     R2,0            ; init edge cond nibble
 
         ; check top edge cond
         mov     R0,R8           ; move row into R0
-        cp      z,0             ; check top edge cond
+        cp      R0,0            ; check top edge cond
         skip    nz,2            ; skip 2 inst if not zero
         bset    R2,3            ; set top edge cond bit
         jr      3               ; jump to page offset check
 
         ; check bottom edge cond
-        cp      z,15            ; check bottom edge cond
+        cp      R0,15           ; check bottom edge cond
         skip    nz,1            ; skip 1 inst if not zero
         bset    R2,2            ; set bottom edge cond bit
-
+        
         ; dispatch based on page offset
         mov     R0,R6           ; move page offset intio R0
-        cp      z,1             ; check page offset
+        cp      R0,1            ; check page offset
         skip    z,1             ; skip 1 inst if zero
         jr      5               ; jump to check right edge cond
         
@@ -135,4 +165,5 @@ rules:
         bset    R2,0            ; set right edge cond
 
         ; applying rules
+        and     R0,0xF          ; Clear carry flag
         ret     R0,0
